499c60c10b9c15b650869fa99490a49419d6d5be
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-05-23 12:53:13 -0500

    Added an API using the useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [pokemonCharacters, setPokemonCharacters] = useState([])
+
+useEffect (() => {
+  axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+    .then((response) => {
+      setPokemonCharacters(response.data.results)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Pokemon Characters </h1>
+    {pokemonCharacters.map((character, index) => ((
+      (<li key={index}>{character.name}</li>))
+    ))}
     </div>
   );
 }
